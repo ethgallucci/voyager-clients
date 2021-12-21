b6c1c8e9adfb5ba7d7b4842d8e0337a0299ed020
ethgallucci <ethgallucci@gmail.com>
2021-12-21 12:45:58 -0800

    documentation for timing & keys modules

--- src/lib/to_pretty.rs
+++
@@ -1,9 +0,0 @@
-use serde_json::{Value as JsonValue};
-use std::error::Error;
-
-pub fn to_string_pretty(res: String) -> Result<String, Box<dyn Error>> {
-    let json: JsonValue = serde_json::from_str(&res).unwrap();
-    let pretty: String = serde_json::to_string_pretty(&json).unwrap();
-
-    Ok(pretty)
-}
