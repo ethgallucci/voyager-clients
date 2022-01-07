7f890e611e9ea0bbde4bc61361b21f689d6b450c
ethgallucci <ethgallucci@gmail.com>
2022-01-06 20:03:12 -0800

    preparing struct overhaul for apis and allowing dynamic querys

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
-//! 
+//!
 //! # Sample Program with voyager
 //! ```
 //! use voyager_client::*;
-//! 
+//!
 //! // Run this function only once
 //! keys::set_key("[YOUR_API_KEY]");
-//! 
+//!
 //! let magnetic_storms = weather::magnetic().unwrap();
 //! ```
 //! This will fetch a response from the magnetic storms endpoint, and convert it
@@ -13,33 +13,33 @@
 
 #![allow(dead_code)]
 
-pub use keys::*;
-pub use timing::*;
 pub use bar::*;
-pub use to_pretty::*;
-pub use apod::*;
-pub use weather::*;
+pub use keys::*;
 pub use neo::*;
+pub use timing::*;
+pub use to_pretty::*;
+pub use weather::*;
+pub use APOD::*;
 
 /// Handling API keys for NASA's open APIs.
 /// Includes methods for storing, and retrieving keys for any user
-/// 
+///
 /// # Configuring a key
 /// ```
 /// set_key("[YOUR_API_KEY]")?;
 /// ```
-/// 
+///
 /// # Retrieving a key
 /// ```
 /// let key = get_key()?;
 /// ```
 pub mod keys {
-    use std::fs;
     use std::error::Error;
+    use std::fs;
     use std::fs::File;
     use std::io::prelude::*;
 
-    use users::{ get_user_by_uid, get_current_uid };
+    use users::{get_current_uid, get_user_by_uid};
 
     /// Stores a key.
     pub fn set_key(key: &str) -> std::io::Result<()> {
@@ -58,9 +58,11 @@ pub mod keys {
     /// Retrieves a key
     pub fn get_key() -> Result<String, Box<dyn Error>> {
         let user = get_user_by_uid(get_current_uid()).unwrap();
-        let path_to_key = format!("/Users/{}/voyager/.api_key.txt", user.name().to_string_lossy());
-        let key = fs::read_to_string(&path_to_key)
-            .expect("Couldn't read api key");
+        let path_to_key = format!(
+            "/Users/{}/voyager/.api_key.txt",
+            user.name().to_string_lossy()
+        );
+        let key = fs::read_to_string(&path_to_key).expect("Couldn't read api key");
 
         Ok(key)
     }
@@ -73,8 +75,7 @@ pub mod keys {
     }
 }
 
-
-/// For handling different request timings. Methods for formatting one day requests, one week, 
+/// For handling different request timings. Methods for formatting one day requests, one week,
 /// two week, and one month requests into a String format.
 ///
 /// # Query in a one month range
@@ -94,7 +95,7 @@ pub mod timing {
     /// # Example
     /// ```
     /// let today = timing::today();
-    /// 
+    ///
     /// assert_eq!("2021-12-21", today);
     /// ```
     pub fn today() -> String {
@@ -133,8 +134,8 @@ pub mod timing {
     }
 }
 
-/// Displaying progress bar in the terminal as part of the CLI 
-/// 
+/// Displaying progress bar in the terminal as part of the CLI
+///
 /// # Setup a progress bar
 /// ```
 /// let byte_length = response.as_bytes().len();
@@ -160,17 +161,17 @@ pub mod bar {
 }
 
 /// Contains methods for prettyfying JSON responses.
-/// 
+///
 /// # Example
-/// 
+///
 /// ```
 /// let res: String = ureq::get(&url).call()?.into_string()?;
-/// let neo = to_string_pretty(res).unwrap(); 
+/// let neo = to_string_pretty(res).unwrap();
 /// ```
 /// This will output a prettyfied String (JSON format) response instead of a large unformatted JSON blob
-/// 
+///
 pub mod to_pretty {
-    use serde_json::{Value as JsonValue};
+    use serde_json::Value as JsonValue;
     use std::error::Error;
 
     /// Converts a JSON blob into a pretty string, easily readible.
@@ -183,75 +184,121 @@ pub mod to_pretty {
 }
 
 /// For interacting with NASA's Picture of the Day endpoint.
-/// 
+///
 /// # Querying APOD endpoint
-/// 
+///
 /// ```
 /// let apod = apod().unwrap();
 /// println!("{}", apod)
 /// ```
 /// This will return a response containing data on the Picture of the Day, as well as the url to the jpg file.
-/// the println! is not necessary, because each query method includes it's own print statement as part of the 
-/// progress bar function call. 
-/// 
-pub mod apod {
+/// the println! is not necessary, because each query method includes it's own print statement as part of the
+/// progress bar function call.
+///
+pub mod APOD {
     use std::error::Error;
 
-    use super::{keys, bar};
+    use super::keys;
     use super::to_pretty::to_string_pretty;
 
-    pub fn get_apod() -> Result<String, Box<dyn Error>> {
+    pub struct apod {
+        base_url: String,
+        date: Option<String>,
+    }
+
+    impl apod {
+        pub fn new() -> Self {
+            apod {
+                base_url: String::from("https://api.nasa.gov/planetary/apod?"),
+                date: None,
+            }
+        }
+
+        pub fn set_date(&mut self, date: String) {
+            self.date = Some(date);
+        }
+
+        pub fn query(&self) -> Result<String, Box<dyn Error>> {
+            let key: String = keys::get_key()?;
+
+            if self.date.is_none() {
+                let url = format!("{}api_key={}", self.base_url, key);
+
+                let res: String = ureq::get(&url).call()?.into_string()?;
+                let apod = to_string_pretty(res).unwrap();
+
+                Ok(apod)
+            }
+            else {
+                let date = self.date.as_ref().unwrap();
+                let url = format!("{}date={}&api_key={}", self.base_url, date, key);
+
+                let res: String = ureq::get(&url).call()?.into_string()?;
+                let apod = to_string_pretty(res).unwrap();
+
+                Ok(apod)
+            }
+        }
+    }
+
+    /*pub fn get_apod() -> Result<String, Box<dyn Error>> {
         let key: String = keys::get_key()?;
         let url: String = format!("https://api.nasa.gov/planetary/apod?api_key={}", key);
 
         let res: String = ureq::get(&url).call()?.into_string()?;
-        let apod = to_string_pretty(res).unwrap(); 
+        let apod = to_string_pretty(res).unwrap();
 
         Ok(apod)
     }
+    */
 }
 
 /// Contains methods for interacting with Solar Flare and Magnetic Storm endpoints.
-/// 
+///
 /// # Querying solar flare API
-/// 
+///
 /// ```
 /// let sflare = sflare().unwrap();
 /// ```
 /// all API query functions will pipe out their response into the progress bar method
 /// which will in turn print the response after it's finished processing.
-/// 
+///
 /// # Querying magnetic storm endpoints
-/// 
+///
 /// ```
 /// let mag = magnetic().unwrap();
 /// ```
-/// 
+///
 pub mod weather {
     use std::error::Error;
 
-    use super::{timing, keys, bar};
     use super::to_pretty::to_string_pretty;
+    use super::{bar, keys, timing};
 
     pub fn sflare() -> Result<String, Box<dyn Error>> {
         let now = timing::today();
         let start = timing::two_weeks();
         println!("Starting query from {} to {}", start, now);
-        
+
         let key: String = keys::get_key().unwrap();
-        let url: String = format!("https://api.nasa.gov/DONKI/FLR?startDate={}&endDate={}&api_key={}", start, now, key);
-        
+        let url: String = format!(
+            "https://api.nasa.gov/DONKI/FLR?startDate={}&endDate={}&api_key={}",
+            start, now, key
+        );
+
         let res: String = ureq::get(&url).call()?.into_string()?;
         let sflare = to_string_pretty(res).unwrap();
-        
+
         Ok(sflare)
     }
 
     pub fn magnetic() -> Result<String, Box<dyn Error>> {
-        
         let key: String = keys::get_key().unwrap();
-        let url: String = format!("https://api.nasa.gov/DONKI/GST?startDate=2021-01-01&endDate=2021-12-10&api_key={}", key);
-        
+        let url: String = format!(
+            "https://api.nasa.gov/DONKI/GST?startDate=2021-01-01&endDate=2021-12-10&api_key={}",
+            key
+        );
+
         let res: String = ureq::get(&url).call()?.into_string()?;
         let magnetic = to_string_pretty(res).unwrap();
 
@@ -260,7 +307,7 @@ pub mod weather {
 }
 
 /// For interacting with the Near Earth Objects API.
-/// 
+///
 /// # Example
 /// ```
 /// let neo = neo().unwrap()
@@ -271,17 +318,20 @@ pub mod weather {
 pub mod neo {
     use std::error::Error;
 
-    use super::{timing, keys, bar};
     use super::to_pretty::to_string_pretty;
+    use super::{bar, keys, timing};
 
     pub fn neo() -> Result<String, Box<dyn Error>> {
         let start = timing::one_day();
         let now = timing::today();
         println!("Starting query from {} to {}", start, now);
-        
+
         let key = keys::get_key()?;
-        let url = format!("https://api.nasa.gov/neo/rest/v1/feed?start_date={}&end_date={}&api_key={}", start, now, key);
-        
+        let url = format!(
+            "https://api.nasa.gov/neo/rest/v1/feed?start_date={}&end_date={}&api_key={}",
+            start, now, key
+        );
+
         let res: String = ureq::get(&url).call()?.into_string()?;
         let neo = to_string_pretty(res).unwrap();
 
--- src/main.rs
+++ src/main.rs
@@ -7,39 +7,10 @@ use argparse::*;
 mod lib;
 use lib::*;
 
-fn main() -> Result<(), Box<dyn Error>> {
-    // Recollect the enviroment arguments
-    let args: Vec<String> = env::args().collect();
-    let command = argparse().unwrap();
-    // Before jumping into the match arm, first check if the command is a config command
-    if command == Arg::SETKEY {
-        // Set the API key
-        keys::set_key(&args[3])?;
-        Ok(())
-    }
-    else if command == Arg::GETKEY {
-        let key = keys::get_key()?;
-        println!("key: {}", key);
-        Ok(())
-    }
+fn main() {
+    let mut base = apod::new();
+    base.set_date(String::from("2015-06-06"));
+    let pic = base.query().unwrap();
 
-    // Command is not a config command - match on command and output the _response
-    else {
-        let _res: String;
-        match command {
-            Arg::SFLARE => _res = weather::sflare().unwrap(),
-            Arg::MAG => _res = weather::magnetic().unwrap(),
-            Arg::APOD => _res = get_apod().unwrap(),
-            Arg::NEO => _res = neo().unwrap(),
-            // Default to apod if command can't be parsed
-            Arg::BADCOMMAND => _res = get_apod().unwrap(),
-            _ => panic!()
-        };
-        if command == Arg::BADCOMMAND {
-            println!("\nDefaulted to APOD upon Bad Command\n\n")
-        }
-        Ok(())
-    }
-
-
+    println!("{}", pic);
 }
