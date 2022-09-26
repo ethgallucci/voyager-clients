6c2044a0f77edda90854d7761e7face9e3c4d162
staticsemaphore <staticanne@skiff.com>
2022-09-26 09:41:34 -0700

    interface

--- src/client.rs
+++ src/client.rs
@@ -1,25 +1,61 @@
-use serde_json::Value as JsonValue;
+use serde::{Deserialize, Serialize};
 
-/// A base API client. Contains no abstraction to the Open APIs, but contains the base url to
-/// the Open API endpoint, and the last response recieved if there was any.
-#[derive(Debug, Clone, PartialEq)]
-pub struct Base {
-    pub burl: String,
-    pub lastr: Option<JsonValue>,
+pub trait Client {
+    fn base(&self) -> &BaseBind;
+    fn make_query_str(&self, query: &str) -> String;
 }
 
-/// Defines behavior of the Base struct
-pub trait BaseLayer {
-    fn burl_update(&mut self, burl: String) -> ();
-    fn lastr(&self) -> Option<JsonValue>;
+pub trait Interface {
+    type Params;
+
+    fn start(base: BaseBind) -> ClientBind;
+    fn query(&self, params: Self::Params) -> String;
 }
 
-impl BaseLayer for Base {
-    fn burl_update(&mut self, burl: String) -> () {
-        self.burl = burl
+#[derive(Debug, Deserialize, Serialize)]
+pub struct BaseBind(String);
+
+impl BaseBind {
+    pub fn url(&self) -> &str {
+        &self.0
+    }
+}
+
+#[derive(Debug)]
+pub struct ClientBind {
+    base: BaseBind,
+    target: Option<String>,
+    /// Last (Query, Response)
+    #[allow(dead_code)]
+    lqr: Option<(String, String)>,
+}
+
+impl ClientBind {
+    pub fn new(
+        base: BaseBind,
+        target: Option<String>,
+        lqr: Option<(String, String)>,
+    ) -> ClientBind {
+        ClientBind { base, target, lqr }
     }
 
-    fn lastr(&self) -> Option<JsonValue> {
-        self.lastr.clone()
+    pub fn new_base(&mut self, base: BaseBind) {
+        self.base = base;
+    }
+
+    pub fn set_target(&mut self, target: Option<String>) {
+        self.target = target;
+    }
+}
+
+impl Client for ClientBind {
+    fn base(&self) -> &BaseBind {
+        &self.base
+    }
+
+    fn make_query_str(&self, query: &str) -> String {
+        let mut url = self.base.url().to_string();
+        url.push_str(&query);
+        url
     }
 }
